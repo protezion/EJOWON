4c85c16018e6b71b970f09edf1627ef1ce28ac2a
protezion <oxij@naver.com>
2017-08-29 16:26:18 +0900

    프로젝트 등록
    등록

---
+++ app/src/main/java/kr/or/kosta/ejowon/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.or.kosta.ejowon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 이게 보이면 제대로 된 겁니다.
+        // 이제 시작
+    }
+}
